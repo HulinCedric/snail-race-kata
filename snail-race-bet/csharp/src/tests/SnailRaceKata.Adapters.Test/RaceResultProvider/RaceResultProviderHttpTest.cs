663fd8c982cc9650f5592f1b53769da8e8d43920
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-14 19:30:52 +0200

    Record and Playback api interaction with Wiremock

--- snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderHttpTest.cs
+++ snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderHttpTest.cs
@@ -1,13 +1,42 @@
 using SnailRaceKata.Adapters.RaceResultProvider;
+using WireMock.Handlers;
+using WireMock.Server;
+using WireMock.Settings;
 
 namespace SnailRaceKata.Test.Adapters.RaceResultProvider;
 
 public class RaceResultProviderHttpTest : RaceResultProviderContractTest, IDisposable
 {
-    private readonly HttpClient _httpClient = new() { BaseAddress = new Uri("http://localhost:8000") };
+    private readonly HttpClient _httpClient;
     private readonly Domain.RaceResultProvider _provider;
 
-    public RaceResultProviderHttpTest() => _provider = new RaceResultProviderHttp(_httpClient);
+    private readonly WireMockServerSettings _recordApiSettings = new()
+    {
+        ProxyAndRecordSettings = new ProxyAndRecordSettings
+        {
+            Url = "http://localhost:8000",
+            SaveMapping = true,
+            SaveMappingToFile = true,
+            SaveMappingForStatusCodePattern = "2xx",
+            ExcludedHeaders = ["Host", "Date"],
+        },
+        FileSystemHandler = new LocalFileSystemHandler("../../../RaceResultProvider/RaceResultApiSnapshots")
+    };
+    
+    private readonly WireMockServerSettings _playbackApiSettings = new()
+    {
+        ReadStaticMappings = true,
+        FileSystemHandler = new LocalFileSystemHandler("../../../RaceResultProvider/RaceResultApiSnapshots")
+    };
+
+    public RaceResultProviderHttpTest()
+    {
+        var wireMockServer = WireMockServer.Start(_playbackApiSettings);
+
+        _httpClient = new HttpClient { BaseAddress = new Uri(wireMockServer.Url!) };
+
+        _provider = new RaceResultProviderHttp(_httpClient);
+    }
 
     public void Dispose() => _httpClient.Dispose();
 
