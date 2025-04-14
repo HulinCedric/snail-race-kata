d087517f8ff7dab9f4bf6bd19b1c1facf7534913
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-14 15:38:40 +0200

    Drive RaceResultProvider result

--- snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderContractTest.cs
+++ snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderContractTest.cs
@@ -11,6 +11,14 @@ public abstract class RaceResultProviderContractTest
     {
         var races = await GetProvider().Races();
 
-        races.Races.Should().NotBeEmpty();
+        races.Races.Should()
+            .ContainEquivalentOf(
+                new Domain.RaceResultProvider.SnailRace(
+                    RaceId: 530572,
+                    Timestamp: 1744614752426L,
+                    new Domain.RaceResultProvider.Podium(
+                        First: new Domain.RaceResultProvider.Snail(2, "Man O'War"),
+                        Second: new Domain.RaceResultProvider.Snail(7, "Frankel"),
+                        Third: new Domain.RaceResultProvider.Snail(3, "Seabiscuit"))));
     }
 }
--- snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderFakeTest.cs
+++ snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderFakeTest.cs
@@ -4,7 +4,21 @@ namespace SnailRaceKata.Test.Adapters.RaceResultProvider;
 
 public class RaceResultProviderFakeTest : RaceResultProviderContractTest
 {
-    private readonly Domain.RaceResultProvider _provider = new RaceResultProviderFake();
+    private readonly RaceResultProviderFake _provider;
+
+    public RaceResultProviderFakeTest()
+    {
+        _provider = new RaceResultProviderFake();
+
+        _provider.AlreadyContains(
+            new Domain.RaceResultProvider.SnailRace(
+                RaceId: 530572,
+                Timestamp: 1744614752426L,
+                new Domain.RaceResultProvider.Podium(
+                    First: new Domain.RaceResultProvider.Snail(2, "Man O'War"),
+                    Second: new Domain.RaceResultProvider.Snail(7, "Frankel"),
+                    Third: new Domain.RaceResultProvider.Snail(3, "Seabiscuit"))));
+    }
 
     protected override Domain.RaceResultProvider GetProvider() => _provider;
 }
