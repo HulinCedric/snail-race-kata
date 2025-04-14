29c42b746d7b2c742b1d877fecf905d726247e2c
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-14 20:06:41 +0200

    Verify result contract with Verify

--- snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderContractTest.cs
+++ snail-race-bet/csharp/src/tests/SnailRaceKata.Adapters.Test/RaceResultProvider/RaceResultProviderContractTest.cs
@@ -1,29 +1,23 @@
-using FluentAssertions;
-using static SnailRaceKata.Test.Common.Builders.PodiumResultBuilder;
-using static SnailRaceKata.Test.Common.Builders.SnailRaceResultBuilder;
-using static SnailRaceKata.Test.Common.Builders.SnailResultBuilder;
-
 namespace SnailRaceKata.Test.Adapters.RaceResultProvider;
 
 public abstract class RaceResultProviderContractTest
 {
+    private readonly VerifySettings _verifySettings;
+
+    protected RaceResultProviderContractTest()
+    {
+        _verifySettings = new VerifySettings();
+        _verifySettings.UseTypeName(nameof(RaceResultProviderContractTest));
+        _verifySettings.DisableRequireUniquePrefix();
+    }
+
     protected abstract Domain.RaceResultProvider GetProvider();
 
     [Fact]
     public async Task Provide_race_results()
     {
         var races = await GetProvider().Races();
-
-        races.Races.Should()
-            .ContainEquivalentOf(
-                ASnailRaceResult()
-                    .WithRaceId(838056)
-                    .AtTimestamp(1744652903667L)
-                    .OnPodium(
-                        APodium()
-                            .OnFirstPlace(ASnail().Named("Makybe Diva").Numbered(18))
-                            .OnSecondPlace(ASnail().Named("Winx").Numbered(19))
-                            .OnThirdPlace(ASnail().Named("Man O'War").Numbered(2)))
-                    .Build());
+        
+        await Verify(races.Races.Take(1), _verifySettings).ScrubMembers("RaceId", "Timestamp", "Name", "Number");
     }
 }
